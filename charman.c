9b677ba7edef303d5c589fbc7599cf16e9e2526a
cmalkire <cmalkire@alaska.edu>
2017-07-30 08:49:57 -0800

    Making changes to char_info class. Writing charachter creation function.

--- charman.c
+++ charman.c
@@ -12,24 +12,53 @@ using namespace std;
 
 //Character Information Class [These are all things that are just intrinsic to the character, and should be cross-version, and are all decided by the player]
 class char_info {
-
- public:
-	char name[50], age[5], hair[10], eyes[10], deity[10], alignment[20], gender[10];
-	int gametype_var, hft, hin, weight, size_var; //Note: hft stands for height-ft and hin stands for height-in
+	char name[50], hair[10], eyes[10], deity[10], alignment[20], gender[10], race[10]; //Basic Character information, really only a few of these will be referenced
+	int gametype_var, hft, hin, weight, size_var, age; //Note: hft stands for height-ft and hin stands for height-in
 	int Str, Dex, Con, Int, Wis, Cha; //Stats
-
- private:
-
+ //This will hold all the functions that will alter character data
+ public:
+	void create_char();
+	void alter_char();
+	void level_char();
+	void show_char();
+	void list_chars();
 };
 
-//Character Attribute Class [These are things that are likely racial, and will need to be altered based on race and class] (Pathfinder)
-class char_attr {
+//Function Definitions [This is what each of the functions in char_info will perform]
 
-};
+void char_info::create_char() {
+	int x=0;
+	system("cls");
+	cout<<"New character creation menu, please follow the directions to setup your new character:\n";
+	while (x == 0) {
+		cout<<"Please enter their name: ";
+		cin.getline(name,50);
+		system("cls");
+		cout<<"Please enter their race: ";
+		cin>>race;
+		system("cls")
+		cout<<"Please enter their gender: ";
+		cin>>gender;
+		system("cls");
+		cout<<"Please enter their age: ";
+		cin>>age;
+		system("cls");
+		cout<<"Please enter their height in feet: ";
+		cin>>hft;
+		cout<<"Please enter their height in inches ";
+		cin>>hin;
+		system("cls");
+		cout<<"Please enter their hair color: ";
+		cin>>hair;
+		system("cls")
+		cout<<"Please enter their alignment: ";
+		cin.getline(alignment,20);
+		system("cls");
+ }
 
-int main()
+int main() 
 {
-	cout<<"Welcome to cmalkire's Dungeons & Dragons Character manager!\nPress Enter to begin...";
+	cout<<"Welcome to cmalkire's Character Manager!\nPress Enter to continue...\n";
 	cin.get();
-	cout<<"I'm sorry, this program isn't exactly ready yet, and you're not able to view all the neat features that I'm going to put in. I'll print a changelog in future updates.\nFor now, sit tight! and check back later!\n";
+	cout<<"This program will help manage characters in the pathfinder system. At the moment, there's nothing here. Sorry about that.\n";
 }
