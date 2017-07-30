fd4bd216d5586153778b7ef41b84f2656b016752
cmalkire <cmalkire@alaska.edu>
2017-07-29 21:05:21 -0800

    Some organizational things, added some character description definitions, and started shortening names in the code for efficiency

--- charman.c
+++ charman.c
@@ -2,27 +2,27 @@
 Caleb Alkire
 7/29/17
 dnd.c
-This program will hold an arbitrary amount of character for DnD (pathfinder system to start). It will serve as a character manager, and allow leveling up in a simple fashion
+This program will hold an arbitrary amount of character for DnD (pathfinder system to start). It will serve as a character manager, and allow leveling up in a simple fashion.
 */
-//github test
+
 #include <iostream>
 #include <fstream>
 
 using namespace.std;
 
-//Base Class
+//Base Class [These are all things that are just intrinsic to the character, and should be cross-version]
 class char_info {
 
  public:
-	char name[50];
-	int gametype_var;
-	char age[5];
+	char name[50], age[5], hair[10], eyes[10], deity[10];
+	int gametype_var, hft, hin, weight; //Note: hft stands for height-ft and hin stands for height-in
+
  private:
 
 }
 
-//Attributes will go in here (Armor Class, hp, stats, movement, etc)
-class char_attributes: public char_info {
+//Attribute Class [These are more numerical things, such as Stats, movement, etc...] (Pathfinder)
+class attr_path: public char_info {
 
 }
 
